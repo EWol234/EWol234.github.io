d72e951dc4c26b17e4e0650a064b289367dae036
Eyob Woldeghebriel <eyobworrede@gmail.com>
2015-07-30 18:40:13 -0400

    Added CSS and Django functions for project page.

--- website/projects/views.py
+++ website/projects/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'projects/index.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'projects/projects.html', {'posts': posts})
+
+def proj_display(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'projects/proj_display.html', {'post': post})
