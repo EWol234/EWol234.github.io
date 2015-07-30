af6e9f414f18f55100a34be18cde6e31da6a2e35
Eyob Woldeghebriel <eyobworrede@gmail.com>
2015-07-30 16:17:49 -0400

    My first Django commit!

---
+++ website/projects/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=datetime.datetime(2015, 7, 30, 20, 2, 43, 964579, tzinfo=utc))),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+            ],
+        ),
+    ]
