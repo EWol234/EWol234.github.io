cf0bcbb1d6a9a21cfbd722d2871ea8bd01255cba
Eyob Woldeghebriel <eyobworrede@gmail.com>
2015-07-30 22:18:08 -0400

    Added rest of menu pages and matching view and url patterns.

--- website/projects/views.py
+++ website/projects/views.py
@@ -6,8 +6,18 @@ from django.utils import timezone
 
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'projects/projects.html', {'posts': posts})
+    return render(request, 'projects/index.html', {'posts': posts})
 
 def proj_display(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'projects/proj_display.html', {'post': post})
+
+def proj_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'projects/projects.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'projects/about.html', {})
+
+def extras(request):
+    return render(request, 'projects/extras.html', {})
