10d0c3f5827ae9331b3d26e48df575bcd95c83dd
Eyob Woldeghebriel <eyobworrede@gmail.com>
2015-08-02 21:11:54 -0400

    Fixed search function to return No Results page when none are found.

--- website/search/views.py
+++ website/search/views.py
@@ -3,10 +3,15 @@ from projects.models import Post
 
 # Create your views here.
 def list(request):
+    message = ''
     if request.GET['q'] is not '':
         q = request.GET['q']
         posts = Post.objects.filter(title__icontains=request.GET['q'])
-        return render(request, 'search/results.html', {'posts': posts, 'q': q})
+        if not posts:
+            message = 'No results found.'
+            return render(request, 'search/results.html', {'posts': posts, 'q': q, 'message': message})
+        else:
+            return render(request, 'search/results.html', {'posts': posts, 'q': q})
     else:
         message = 'No results found.'
         return render(request, 'search/results.html', {'message': message})
