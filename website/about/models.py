287e466e34e5fec6816181c765cf28255b144c1d
Eyob Woldeghebriel <eyobworrede@gmail.com>
2015-09-18 20:35:06 -0400

    Added categories model to easily update about page.

--- website/about/models.py
+++ website/about/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    order = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- website/about/views.py
+++ website/about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 def about(request):
-    return render(request, "about/about.html", {})
+    categories = Category.objects.all().order_by('order')
+    return render(request, "about/about.html", {'categories': categories,})
